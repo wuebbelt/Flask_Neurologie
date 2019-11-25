a7e3d5d1416dd2f52d5b12f3b885994a1eb7f9ac
Glitch (wuebbelt-flask-neurologie) <none>
2019-11-25 09:08:02 +0000

    🚩😻 Checkpoint ./neurologie.py:8826709/196

--- neurologie.py
+++ neurologie.py
@@ -236,6 +236,9 @@ def home():
 	form = StartForm()
 	if form.validate_on_submit(): 
 		session["patient"] =  "" 
+    session["AbspeichernOK"] = "False"
+    if form.AbspeichernOK.data is True:
+      flash('You agreed!')
 		return redirect(url_for('patient'))
 	return render_template('home.html', form=form)
 
