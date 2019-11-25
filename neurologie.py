768582d3e2805d7c9196ca777a7038b72dcb6973
Glitch (wuebbelt-flask-neurologie) <none>
2019-11-25 08:20:02 +0000

    🙆📻 Checkpoint ./neurologie.py:8826709/22453

--- neurologie.py
+++ neurologie.py
@@ -41,17 +41,23 @@ def calculate_score(data):
 	Schuljahre
 	"""
 
+    data["Gesamt_Score"] = 0
+
 	age = int(data["age"])
 	gender = int(data["gender"])
 	Schuljahre = int(data["Schuljahre"])
 	if data["NCTA"].isnumeric():
 		NCTA = int(data["NCTA"])
-  if data["NCTA"].isnumeric():
-	  NCTB = int(data["NCTB"])
-	LTTTIME = int(data["LTTTIME"])
-	LTTERROR = int(data["LTTERROR"])
-	DST = int(data["DST"])
-	SDOT = int(data["SDOT"])
+	if data["NCTB"].isnumeric():
+		NCTB = int(data["NCTB"])
+	if data["LTTTIME"].isnumeric():
+		LTTTIME = int(data["LTTTIME"])
+	if data["LTTERROR"].isnumeric():
+		LTTERROR = int(data["LTTERROR"])
+	if data["DST"].isnumeric():
+		DST = int(data["DST"])
+	if data["SDOT"].isnumeric():
+		SDOT = int(data["SDOT"])
 
 	# Umkodierung (falls nötig):  Schuljahre in 3 Kategorien
 	# Formal Educarion as contrast variables 
@@ -80,8 +86,6 @@ def calculate_score(data):
 	gender -= 1	
 
 	# NCTA 
-	print(type(data["NCTA"]))
-	
 	if data["NCTA"].isnumeric():
 		aNCTACovarH2019 = 2.641457
 		b_ageNCTACovarH2019 = 0.010871
@@ -102,119 +106,128 @@ def calculate_score(data):
 		if (NCTA <= NCTAm1COVARH2019): NCTAScoreNormCOVARH2019  = +1
 
 		data["NCTA_Score"] = NCTAScoreNormCOVARH2019
+		data["Gesamt_Score"] += NCTAScoreNormCOVARH2019
 
 
 	# NCTB
-	aNCTBCovarH2019 = 3.830604
-	b_ageNCTBCovarH2019 = 0.010027
-	b_FormalEducation2NCTBCovarH2019 = -0.184867
-	b_FormalEducation3NCTBCovarH2019 =-0.34977319  
-	sNCTBCovarH2019 = sqrt(0.07484)
+	if data["NCTB"].isnumeric():
+		aNCTBCovarH2019 = 3.830604
+		b_ageNCTBCovarH2019 = 0.010027
+		b_FormalEducation2NCTBCovarH2019 = -0.184867
+		b_FormalEducation3NCTBCovarH2019 =-0.34977319  
+		sNCTBCovarH2019 = sqrt(0.07484)
 
-	NCTB0COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 0 *sNCTBCovarH2019  )
-	NCTB1COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 1 * sNCTBCovarH2019  )
-	NCTB2COVARH2019  = exp(aNCTBCovarH2019 + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 2* sNCTBCovarH2019 )
-	NCTB3COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 3 * sNCTBCovarH2019  )
-	NCTBm1COVARH2019 = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 - 1 * sNCTBCovarH2019 )
+		NCTB0COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 0 *sNCTBCovarH2019  )
+		NCTB1COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 1 * sNCTBCovarH2019  )
+		NCTB2COVARH2019  = exp(aNCTBCovarH2019 + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 2* sNCTBCovarH2019 )
+		NCTB3COVARH2019  = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 + 3 * sNCTBCovarH2019  )
+		NCTBm1COVARH2019 = exp(aNCTBCovarH2019  + b_ageNCTBCovarH2019 *age + b_FormalEducation2NCTBCovarH2019 * FormalEducation2    + b_FormalEducation3NCTBCovarH2019 * FormalEducation3 - 1 * sNCTBCovarH2019 )
 
-	NCTBScoreNormCOVARH2019= 0.
-	if (NCTB >= NCTB1COVARH2019 ): NCTBScoreNormCOVARH2019 = -1
-	if (NCTB >= NCTB2COVARH2019 ): NCTBScoreNormCOVARH2019  = -2
-	if (NCTB >= NCTB3COVARH2019 ): NCTBScoreNormCOVARH2019 = -3
-	if (NCTB <= NCTBm1COVARH2019): NCTBScoreNormCOVARH2019  = +1
+		NCTBScoreNormCOVARH2019= 0.
+		if (NCTB >= NCTB1COVARH2019 ): NCTBScoreNormCOVARH2019 = -1
+		if (NCTB >= NCTB2COVARH2019 ): NCTBScoreNormCOVARH2019  = -2
+		if (NCTB >= NCTB3COVARH2019 ): NCTBScoreNormCOVARH2019 = -3
+		if (NCTB <= NCTBm1COVARH2019): NCTBScoreNormCOVARH2019  = +1
 
-	data["NCTB_Score"] = NCTBScoreNormCOVARH2019
+		data["NCTB_Score"] = NCTBScoreNormCOVARH2019
+		data["Gesamt_Score"] += NCTBScoreNormCOVARH2019
 
 	
 	# LTT Time 
-	aLTTTIMECovarH2019 = 4.131675
-	b_ageLTTTIMECovarH2019 = 0.003448
-	b_FormalEducation2LTTTIMECovarH2019 = 0
-	b_FormalEducation3LTTTIMECovarH2019=  0
-	sLTTTIMECovarH2019 = sqrt(0.077122)
+	if data["LTTTIME"].isnumeric():
+		aLTTTIMECovarH2019 = 4.131675
+		b_ageLTTTIMECovarH2019 = 0.003448
+		b_FormalEducation2LTTTIMECovarH2019 = 0
+		b_FormalEducation3LTTTIMECovarH2019=  0
+		sLTTTIMECovarH2019 = sqrt(0.077122)
 
-	LTTTIME0COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 0 *sLTTTIMECovarH2019  )
-	LTTTIME1COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 1 * sLTTTIMECovarH2019  )
-	LTTTIME2COVARH2019  = exp(aLTTTIMECovarH2019 + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 2* sLTTTIMECovarH2019 )
-	LTTTIME3COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 3 * sLTTTIMECovarH2019  )
-	LTTTIMEm1COVARH2019 = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 - 1 * sLTTTIMECovarH2019 )
+		LTTTIME0COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 0 *sLTTTIMECovarH2019  )
+		LTTTIME1COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 1 * sLTTTIMECovarH2019  )
+		LTTTIME2COVARH2019  = exp(aLTTTIMECovarH2019 + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 2* sLTTTIMECovarH2019 )
+		LTTTIME3COVARH2019  = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 + 3 * sLTTTIMECovarH2019  )
+		LTTTIMEm1COVARH2019 = exp(aLTTTIMECovarH2019  + b_ageLTTTIMECovarH2019 *age + b_FormalEducation2LTTTIMECovarH2019 * FormalEducation2    + b_FormalEducation3LTTTIMECovarH2019 * FormalEducation3 - 1 * sLTTTIMECovarH2019 )
 
-	LTTTIMEScoreNormCOVARH2019= 0
-	if (LTTTIME >= LTTTIME1COVARH2019 ): LTTTIMEScoreNormCOVARH2019 = -1
-	if (LTTTIME >= LTTTIME2COVARH2019 ): LTTTIMEScoreNormCOVARH2019  = -2
-	if (LTTTIME >= LTTTIME3COVARH2019 ): LTTTIMEScoreNormCOVARH2019 = -3
-	if (LTTTIME <= LTTTIMEm1COVARH2019): LTTTIMEScoreNormCOVARH2019  = +1
+		LTTTIMEScoreNormCOVARH2019= 0
+		if (LTTTIME >= LTTTIME1COVARH2019 ): LTTTIMEScoreNormCOVARH2019 = -1
+		if (LTTTIME >= LTTTIME2COVARH2019 ): LTTTIMEScoreNormCOVARH2019  = -2
+		if (LTTTIME >= LTTTIME3COVARH2019 ): LTTTIMEScoreNormCOVARH2019 = -3
+		if (LTTTIME <= LTTTIMEm1COVARH2019): LTTTIMEScoreNormCOVARH2019  = +1
 
-	data["LTTTIME_Score"] = LTTTIMEScoreNormCOVARH2019
+		data["LTTTIME_Score"] = LTTTIMEScoreNormCOVARH2019
+		data["Gesamt_Score"] += LTTTIMEScoreNormCOVARH2019
 
 
 	# LTT Error
-	aLTTERRORCovarH2019 = 2.558560 
-	b_ageLTTERRORCovarH2019 = 0.051284
-	b_FormalEducation2LTTERRORCovarH2019 = 0
-	b_FormalEducation3LTTERRORCovarH2019=  0
-	sLTTERRORCovarH2019 = sqrt( 3.860729)
+	if data["LTTERROR"].isnumeric():	
+		aLTTERRORCovarH2019 = 2.558560 
+		b_ageLTTERRORCovarH2019 = 0.051284
+		b_FormalEducation2LTTERRORCovarH2019 = 0
+		b_FormalEducation3LTTERRORCovarH2019=  0
+		sLTTERRORCovarH2019 = sqrt( 3.860729)
 
-	LTTERROR0COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 0 *sLTTERRORCovarH2019  )**2
-	LTTERROR1COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 1 * sLTTERRORCovarH2019  )**2
-	LTTERROR2COVARH2019  = (aLTTERRORCovarH2019 + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 2* sLTTERRORCovarH2019 )**2
-	LTTERROR3COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 3 * sLTTERRORCovarH2019  )**2
-	LTTERRORm1COVARH2019 = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 - 1 * sLTTERRORCovarH2019 )**2
+		LTTERROR0COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 0 *sLTTERRORCovarH2019  )**2
+		LTTERROR1COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 1 * sLTTERRORCovarH2019  )**2
+		LTTERROR2COVARH2019  = (aLTTERRORCovarH2019 + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 2* sLTTERRORCovarH2019 )**2
+		LTTERROR3COVARH2019  = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 + 3 * sLTTERRORCovarH2019  )**2
+		LTTERRORm1COVARH2019 = (aLTTERRORCovarH2019  + b_ageLTTERRORCovarH2019 *age + b_FormalEducation2LTTERRORCovarH2019 * FormalEducation2    + b_FormalEducation3LTTERRORCovarH2019 * FormalEducation3 - 1 * sLTTERRORCovarH2019 )**2
 
-	LTTERRORScoreNormCOVARH2019= 0
-	if (LTTERROR >= LTTERROR1COVARH2019 ): LTTERRORScoreNormCOVARH2019 = -1
-	if (LTTERROR >= LTTERROR2COVARH2019 ): LTTERRORScoreNormCOVARH2019  = -2
-	if (LTTERROR >= LTTERROR3COVARH2019 ): LTTERRORScoreNormCOVARH2019 = -3
-	if (LTTERROR <= LTTERRORm1COVARH2019): LTTERRORScoreNormCOVARH2019  = +1
+		LTTERRORScoreNormCOVARH2019= 0
+		if (LTTERROR >= LTTERROR1COVARH2019 ): LTTERRORScoreNormCOVARH2019 = -1
+		if (LTTERROR >= LTTERROR2COVARH2019 ): LTTERRORScoreNormCOVARH2019  = -2
+		if (LTTERROR >= LTTERROR3COVARH2019 ): LTTERRORScoreNormCOVARH2019 = -3
+		if (LTTERROR <= LTTERRORm1COVARH2019): LTTERRORScoreNormCOVARH2019  = +1
 
-	data["LTTERROR_Score"] = LTTERRORScoreNormCOVARH2019 
-
+		data["LTTERROR_Score"] = LTTERRORScoreNormCOVARH2019 
+		data["Gesamt_Score"] +=  LTTERRORScoreNormCOVARH2019
 
 	#  DST
-	aDSTCovarH2019 = 4.103133
-	b_ageDSTCovarH2019 = -0.006395
-	b_FormalEducation2DSTCovarH2019 = 0.091533
-	b_FormalEducation3DSTCovarH2019=  0.138129
-	b_genderDSTCovarH2019 = 0.076659
-	sDSTCovarH2019 = sqrt(0.025989)
+	if data["DST"].isnumeric():
+		aDSTCovarH2019 = 4.103133
+		b_ageDSTCovarH2019 = -0.006395
+		b_FormalEducation2DSTCovarH2019 = 0.091533
+		b_FormalEducation3DSTCovarH2019=  0.138129
+		b_genderDSTCovarH2019 = 0.076659
+		sDSTCovarH2019 = sqrt(0.025989)
 
-	DST0COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3  + b_genderDSTCovarH2019 * gender + 0 *sDSTCovarH2019  )
-	DST1COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender -  1 * sDSTCovarH2019  )
-	DST2COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender - 2 * sDSTCovarH2019 )
-	DST3COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender - 3 * sDSTCovarH2019  )
-	DSTp1COVARH2019 = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3  + b_genderDSTCovarH2019 * gender + 1 * sDSTCovarH2019 )
+		DST0COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3  + b_genderDSTCovarH2019 * gender + 0 *sDSTCovarH2019  )
+		DST1COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender -  1 * sDSTCovarH2019  )
+		DST2COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender - 2 * sDSTCovarH2019 )
+		DST3COVARH2019  = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3 + b_genderDSTCovarH2019 * gender - 3 * sDSTCovarH2019  )
+		DSTp1COVARH2019 = exp(aDSTCovarH2019 + b_ageDSTCovarH2019 *age + b_FormalEducation2DSTCovarH2019 * FormalEducation2    + b_FormalEducation3DSTCovarH2019 * FormalEducation3  + b_genderDSTCovarH2019 * gender + 1 * sDSTCovarH2019 )
 
-	DSTScoreNormCOVARH2019= 0
-	if (DST <= DST1COVARH2019 ): DSTScoreNormCOVARH2019 = -1
-	if (DST <= DST2COVARH2019 ): DSTScoreNormCOVARH2019  = -2
-	if (DST <= DST3COVARH2019 ): DSTScoreNormCOVARH2019 = -3
-	if (DST >= DSTp1COVARH2019): DSTScoreNormCOVARH2019  = +1
+		DSTScoreNormCOVARH2019= 0
+		if (DST <= DST1COVARH2019 ): DSTScoreNormCOVARH2019 = -1
+		if (DST <= DST2COVARH2019 ): DSTScoreNormCOVARH2019  = -2
+		if (DST <= DST3COVARH2019 ): DSTScoreNormCOVARH2019 = -3
+		if (DST >= DSTp1COVARH2019): DSTScoreNormCOVARH2019  = +1
 
-	data["DST_Score"] = DSTScoreNormCOVARH2019
+		data["DST_Score"] = DSTScoreNormCOVARH2019
+		data["Gesamt_Score"] += DSTScoreNormCOVARH2019
 
 
 	# SDOT	
-	aSDOTCovarH2019 = 1.259429
-	b_ageSDOTCovarH2019 = 0.001204
-	b_FormalEducation23SDOTCovarH2019 = -0.026057 
-	sSDOTCovarH2019 = sqrt(0.001729)
+	if data["SDOT"].isnumeric():	
+		aSDOTCovarH2019 = 1.259429
+		b_ageSDOTCovarH2019 = 0.001204
+		b_FormalEducation23SDOTCovarH2019 = -0.026057 
+		sSDOTCovarH2019 = sqrt(0.001729)
 
-	SDOT0COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age + b_FormalEducation23SDOTCovarH2019 * FormalEducation23    + 0 *sSDOTCovarH2019  ))
-	SDOT1COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age + b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 1 * sSDOTCovarH2019  ))
-	SDOT2COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +  b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 2* sSDOTCovarH2019 ))
-	SDOT3COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +  b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 3 * sSDOTCovarH2019  ))
-	SDOTm1COVARH2019 = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +b_FormalEducation23SDOTCovarH2019 * FormalEducation23   - 1 * sSDOTCovarH2019 ))
+		SDOT0COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age + b_FormalEducation23SDOTCovarH2019 * FormalEducation23    + 0 *sSDOTCovarH2019  ))
+		SDOT1COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age + b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 1 * sSDOTCovarH2019  ))
+		SDOT2COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +  b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 2* sSDOTCovarH2019 ))
+		SDOT3COVARH2019  = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +  b_FormalEducation23SDOTCovarH2019 * FormalEducation23   + 3 * sSDOTCovarH2019  ))
+		SDOTm1COVARH2019 = exp(exp(aSDOTCovarH2019 + b_ageSDOTCovarH2019 *age +b_FormalEducation23SDOTCovarH2019 * FormalEducation23   - 1 * sSDOTCovarH2019 ))
 
-	SDOTScoreNormCOVARH2019= 0
-	if (SDOT >= SDOT1COVARH2019 ): SDOTScoreNormCOVARH2019 = -1
-	if (SDOT >= SDOT2COVARH2019 ): SDOTScoreNormCOVARH2019  = -2
-	if (SDOT >= SDOT3COVARH2019 ): SDOTScoreNormCOVARH2019 = -3
-	if (SDOT <= SDOTm1COVARH2019): SDOTScoreNormCOVARH2019  = +1
+		SDOTScoreNormCOVARH2019= 0
+		if (SDOT >= SDOT1COVARH2019 ): SDOTScoreNormCOVARH2019 = -1
+		if (SDOT >= SDOT2COVARH2019 ): SDOTScoreNormCOVARH2019  = -2
+		if (SDOT >= SDOT3COVARH2019 ): SDOTScoreNormCOVARH2019 = -3
+		if (SDOT <= SDOTm1COVARH2019): SDOTScoreNormCOVARH2019  = +1
 
-	data["SDOT_Score"] = SDOTScoreNormCOVARH2019 
+		data["SDOT_Score"] = SDOTScoreNormCOVARH2019
+		data["Gesamt_Score"] += SDOTScoreNormCOVARH2019
 
 	return data
-
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
